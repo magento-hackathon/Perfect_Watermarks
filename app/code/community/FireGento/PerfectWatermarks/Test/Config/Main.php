940995a3064b2718401044906c5e4209d21d0441
Karl Spies <Karl.Spies@flagbit.de>
2013-01-13 10:54:54 +0100

    Fixed modman & added test cases

---
+++ app/code/community/FireGento/PerfectWatermarks/Test/Config/Main.php
@@ -0,0 +1,5 @@
+<?php
+class FireGento_PerfectWatermarks_Test_Main extends EcomDev_PHPUnit_Test_Case
+{
+
+}
---
+++ app/code/community/FireGento/PerfectWatermarks/Test/Model/Product/Image.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: karl
+ * Date: 1/13/13
+ * Time: 10:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
