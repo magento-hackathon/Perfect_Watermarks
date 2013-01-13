ded51a20b00f1620b1171d1b3c11929c955ab1f1
Karl Spies <Karl.Spies@flagbit.de>
2013-01-13 10:56:07 +0100

    Added unit tests to configuration and rewritten class

--- app/code/community/FireGento/PerfectWatermarks/Test/Config/Main.php
+++ app/code/community/FireGento/PerfectWatermarks/Test/Config/Main.php
@@ -1,5 +1,8 @@
 <?php
-class FireGento_PerfectWatermarks_Test_Main extends EcomDev_PHPUnit_Test_Case
+class FireGento_PerfectWatermarks_Test_Config_Main extends EcomDev_PHPUnit_Test_Case_Config
 {
-
+    public function testCatalogProductImageRewriteConfigurationDefined()
+    {
+        return $this->assertModelAlias('catalog/product_image', 'FireGento_PerfectWatermarks_Model_Product_Image');
+    }
 }
--- app/code/community/FireGento/PerfectWatermarks/Test/Model/Product/Image.php
+++ app/code/community/FireGento/PerfectWatermarks/Test/Model/Product/Image.php
@@ -1,8 +1,24 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: karl
- * Date: 1/13/13
- * Time: 10:19 AM
- * To change this template use File | Settings | File Templates.
- */
+
+class FireGento_PerfectWatermarks_Test_Model_Product_Image
+    extends EcomDev_PHPUnit_Test_Case
+{
+    protected $_model;
+
+    protected function setUp()
+    {
+        $this->_model = Mage::getModel('catalog/product_image');
+    }
+
+    public function testReturnsCorrectImageProcessorClass()
+    {
+        /** @var $image Varien_Image */
+        $image = $this->_model->getImageProcessor();
+        $adapterClass = EcomDev_Utils_Reflection::invokeRestrictedMethod(
+            $image, '_getAdapter'
+        );
+
+        $this->assertInstanceOf('Varien_Image_Adapter_Imagemagic',
+            $adapterClass);
+    }
+}
