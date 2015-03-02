54fcae37bfbc2f645e0fd87c1adcd57cf68576c6
Richard Brown <r.brown@lawson-his.co.uk>
2015-03-02 12:18:48 +0000

    Convert CYMK images to RGB
    Some of our images are in CMYK, when they're resized onto a different shape
    canvas, the canvas colour inverts when the colorspace is changed. This patch
    tries to convert the CMYK images to sRGB before compositing them. The conversion
    code was taken from http://php.net/manual/en/imagick.setimagecolorspace.php

--- src/app/code/community/Varien/Image/Adapter/Imagemagic.php
+++ src/app/code/community/Varien/Image/Adapter/Imagemagic.php
@@ -158,6 +158,24 @@ class Varien_Image_Adapter_Imagemagic extends Varien_Image_Adapter_Abstract
             }
             $composite->newimage($frameWidth, $frameHeight, $bgColor);
             $composite->setimageformat($imagick->getimageformat());
+
+            if($imagick->getimagecolorspace() == Imagick::COLORSPACE_CMYK) {
+                $profiles = $imagick->getimageprofiles('*', false);
+                // we're only interested if ICC profile(s) exist
+                $has_icc_profile = (array_search('icc', $profiles) !== false);
+                // if it doesnt have a CMYK ICC profile, we add one
+                if ($has_icc_profile === false) {
+                    $icc_cmyk = file_get_contents(__DIR__ . '/icc_profiles/USWebUncoated.icc');
+                    $imagick->profileImage('icc', $icc_cmyk);
+                    unset($icc_cmyk);
+                }
+                // then we add an RGB profile
+                $icc_rgb = file_get_contents(__DIR__ . '/icc_profiles/sRGB.icc');
+                $imagick->profileImage('icc', $icc_rgb);
+                unset($icc_rgb);
+                $imagick->setimagecolorspace(Imagick::COLORSPACE_SRGB);
+            }
+
             $composite->setimagecolorspace($imagick->getimagecolorspace());
             $dstX = floor(($frameWidth - $imagick->getimagewidth()) / 2);
             $dstY = floor(($frameHeight - $imagick->getimageheight()) / 2);
