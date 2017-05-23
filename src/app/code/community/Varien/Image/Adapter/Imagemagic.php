f1e4552049ef1973919ceb761b63fa07a0206b75
Colin Mollenhour <colin@mollenhour.com>
2017-05-22 22:03:01 -0400

    Fix PHP fatal error for older versions.

--- src/app/code/community/Varien/Image/Adapter/Imagemagic.php
+++ src/app/code/community/Varien/Image/Adapter/Imagemagic.php
@@ -23,7 +23,9 @@ class Varien_Image_Adapter_Imagemagic extends Varien_Image_Adapter_Abstract
     {
         if ($this->_imageHandler === null) {
             // Set tmp path since Imagick apparently does not choose it well (according to auditd file access errors)
-            Imagick::setRegistry('temporary-path', Mage::getBaseDir('tmp'));
+            if (method_exists('Imagick','setRegistry')) {
+                Imagick::setRegistry('temporary-path', Mage::getBaseDir('tmp'));
+            }
             $version = Imagick::getVersion();
             if (strpos($version['versionString'], 'ImageMagick 6.7.') === 0) {
                 chdir(Mage::getBaseDir('tmp')); // Old versions don't use temporary-path but instead the cwd
